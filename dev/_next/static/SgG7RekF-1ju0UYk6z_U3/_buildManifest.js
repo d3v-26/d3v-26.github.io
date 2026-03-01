a70633408154071b7813254d02bf56cac1b63ec8
d3v-26 <68773259+d3v-26@users.noreply.github.com>
2026-03-01 01:49:30 +0000

    Deploying to gh-pages from @ d3v-26/d3v-26.github.io@e9d2212e738da2ab467f0ae24b9665126e325e00 🚀

--- dev/_next/static/SgG7RekF-1ju0UYk6z_U3/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/b637e9a5-96758481f1d32622.js","static/chunks/25-97f3798b7d6a410b.js","static/chunks/pages/index-6b3d091fb3685eec.js"],"/_error":["static/chunks/pages/_error-7a92967bea80186d.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
